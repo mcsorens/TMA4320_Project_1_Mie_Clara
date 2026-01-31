4b940ffcef8ec41cc9a1d6ab20e318fd114ccf6d
clarano30 <clarnord2@gmail.com>
2026-01-31 14:35:43 +0100

    Update run_nn.py

--- scripts/run_nn.py
+++ scripts/run_nn.py
@@ -21,6 +21,30 @@ def main():
     # Oppgave 4.4: Start
     #######################################################################
 
+    os.makedirs("output/nn", exist_ok=True)
+
+    print("Generating training data (FDM + sensor data)...")
+    
+    x, y, t, T_fdm, sensor_data = generate_training_data(cfg)
+
+    print("\nTraining neural network (NN)...")
+    nn_params, losses = train_nn(sensor_data, cfg)
+
+    print("\nPredicting temperature field on full grid using NN...")
+    T_nn = predict_grid(nn_params, x, y, t, cfg)
+
+    print("\nGenerating NN visualizations...")
+    plot_snapshots(
+        x,
+        y,
+        t,
+        T_nn,
+        save_path="output/nn/nn_snapshots.png",
+    )
+    create_animation(
+        x, y, t, T_nn, title="NN", save_path="output/nn/nn_animation.gif"
+
+
     #######################################################################
     # Oppgave 4.4: Slutt
     #######################################################################

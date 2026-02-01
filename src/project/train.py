bfbe4ac0d6c02ca5e35dc5312e6eefcb58df7236
clarano30 <clarnord2@gmail.com>
2026-02-01 12:45:31 +0100

    Update train.py

--- src/project/train.py
+++ src/project/train.py
@@ -35,7 +35,30 @@ def train_nn(
     # Oppgave 4.3: Start
     #######################################################################
 
-    # Update the nn_params and losses dictionary
+    def objective_fn(nn_params, ic_points):
+        l_data = data_loss(nn_params, sensor_data, cfg)
+        l_ic = ic_loss(nn_params, ic_points, cfg)
+
+        l_total = cfg.lambda_data * l_data + cfg.lambda_ic * l_ic
+        return l_total, (l_data, l_ic)
+
+    objective_and_grad = jit(jax.value_and_grad(objective_fn, has_aux=True))
+
+    for _ in tqdm(range(cfg.num_epochs), desc="Training NN"):
+        ic_points, key = sample_ic(key, cfg)
+
+        (loss_total, (loss_data, loss_ic)), grads = objective_and_grad(
+            nn_params, ic_points
+        )
+
+        nn_params, adam_state = adam_step(
+            nn_params, grads, adam_state, lr=cfg.learning_rate
+        )
+
+        losses["total"].append(loss_total)
+        losses["data"].append(loss_data)
+        losses["ic"].append(loss_ic)
+
 
     #######################################################################
     # Oppgave 4.3: Slutt
